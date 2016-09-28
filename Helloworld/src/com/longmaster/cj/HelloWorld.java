fa15a2e5768fd9291e2b292f97def89198eff9c3
cj <584269983@qq.com>
2016-09-28 23:38:05 +0800

    2313

---
+++ Helloworld/src/com/longmaster/cj/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.longmaster.cj;
+
+public class HelloWorld {
+	/**
+	 * java 的main函数必须是static 方法
+	 * @param agrv
+	 */
+	public static void main(String [] agrs){
+		System.out.println("Hello, world");
+		
+		
+	}
+}
